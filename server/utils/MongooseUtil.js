2992698a0fe402797de22da8864e7d7815ed3dc0
mvRon <phatvan12388@gmail.com>
2025-01-18 19:40:04 +0700

    Xoa toan bo du an cu

--- server/utils/MongooseUtil.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const MyConstants = require('./MyConstants');
-const uri = 'mongodb+srv://ronm250304:RoN13141516@cluster0.xn5xe.mongodb.net';
-mongoose.connect(uri, { useNewUrlParser: true })
-    .then(() => { console.log('Connected to ' + MyConstants.DB_SERVER + '/' + MyConstants.DB_DATABASE); })
-    .catch((err) => { console.error(err); });
