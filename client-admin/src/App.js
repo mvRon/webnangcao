a86a5e5ea72ee2479bc4957e082aaa5ff5cd1637
NguyenGiap <nguyengiap211004@gmail.com>
2025-01-18 10:15:49 +0700

    Update App.js

--- client-admin/src/App.js
+++ client-admin/src/App.js
@@ -27,3 +27,21 @@ class App extends Component{
 }
 
 export default App;
+
+import './App.css';
+import React, { Component } from 'react';
+import MyProvider from './contexts/MyProvider';
+import Login from './components/LoginComponent';
+import Main from './components/MainComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+      <Login />
+      <Main />
+      </MyProvider>
+      );
+  }
+}
+export default App;
