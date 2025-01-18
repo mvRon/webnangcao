e1c66c977292e7f7bf6afbd10a9b1c0236833664
mvRon <phatvan12388@gmail.com>
2025-01-18 10:02:16 +0700

    add folder utils va them cac file can thiet

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,13 @@
+const MyConstants = {
+    DB_SERVER : '<cluster>.mongodb.net ',
+    DB_USER : '<db_user >',
+    DB_PASS : '<db_pass >',
+    DB_DATABASE : '<db_name>',
+    EMAIL_USER : '<email_user >', // Microsoft mail service
+    EMAIL_PASS : '<email_pass >',
+    JWT_SECRET : '<jwt_secret >',
+    
+    // tranghongson@gmail.com
+    JWT_EXPIRES : '<jwt_expires >', // in milliseconds
+};
+module . exports = MyConstants ;
