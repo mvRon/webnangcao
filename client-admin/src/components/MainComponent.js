4b00c0139182e4d0946200b5212057356413b188
NguyenGiap <nguyengiap211004@gmail.com>
2025-01-18 10:41:38 +0700

    Update MainComponent.js(Router)

--- client-admin/src/components/MainComponent.js
+++ client-admin/src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react ';
 import MyContext from '../ contexts / MyContext ';
 import Menu from './ MenuComponent ';
 import Home from './ HomeComponent ';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 class Main extends Component {
     static contextType = MyContext; // using this . context to access global state
@@ -10,6 +11,10 @@ class Main extends Component {
             return (
                 < div className="body - admin ">
                     < Menu />
+                    <Routes>
+                        <Route path='/admin' element={<Navigate replace to='/admin/home' />} />
+                        <Route path='/admin/home' element={<Home />} />
+                    </Routes>
                     < Home />
                 </ div >
             );
