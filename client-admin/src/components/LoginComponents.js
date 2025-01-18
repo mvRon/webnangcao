1c1a9fa4d50e136fbab28d56659278f5e73fa580
NguyenGiap <nguyengiap211004@gmail.com>
2025-01-18 10:26:41 +0700

    Update LoginComponents.js

--- client-admin/src/components/LoginComponents.js
+++ client-admin/src/components/LoginComponents.js
@@ -15,56 +15,59 @@ class Login extends Component {
     if (this.context.token === '') {
       return (
         <div className="align-valign-center">
-        <h2 className="text-center">ADMIN LOGIN</h2>
-        <form>
-        <table className="align-center">
-        <tbody>
-        <tr>
-        <td>Username</td>
-        <td><input type="text" value={this.state.txtUsername} onChange={(e) => { this
-      .setState({ txtUsername: e.target.value }) }} /></td>
- 25 </tr>
- 26 <tr>
- 27 <td>Password</td>
- 28 <td><input type="password" value={this.state.txtPassword} onChange={(e) => {
- this.setState({ txtPassword: e.target.value }) }} /></td>
- 29 </tr>
- 30 <tr>
- 31 <td></td>
- 32 <td><input type="submit" value="LOGIN" onClick={(e) => this.btnLoginClick(e)}
- /></td>
- 33 </tr>
- 34 </tbody>
- 35 </table>
- 36 </form>
- 37 </div>
- 38 );
- 39 }
- 40 return (<div />);
- 41 }
- 42 // event-handlers
- 43 btnLoginClick(e) {
- 44 e.preventDefault();
- 45 const username = this.state.txtUsername;
- 46 const password = this.state.txtPassword;
- 47 if (username && password) {
- 48 const account = { username: username, password: password };
- 49 this.apiLogin(account);
- 50 } else {
- 51 alert('Please input username and password');
- 52 }
- 53 }
- 54 // apis
- 55 apiLogin(account) {
- 56 axios.post('/api/admin/login', account).then((res) => {
- 57 const result = res.data;
- 58 if (result.success === true) {
- 59 this.context.setToken(result.token);
- 60 this.context.setUsername(account.username);
- 61 } else {
- 62 alert(result.message);
- 63 }
- 64 });
- 65 }
- 66 }
- 67 export default Login;
+          <h2 className="text-center">ADMIN LOGIN</h2>
+          <form>
+            <table className="align-center">
+              <tbody>
+                <tr>
+                  <td>Username</td>
+                  <td><input type="text" value={this.state.txtUsername} onChange={(e) => {
+                    this
+                      .setState({ txtUsername: e.target.value })
+                  }} /></td>
+                </tr>
+                <tr>
+                  <td>Password</td>
+                  <td><input type="password" value={this.state.txtPassword} onChange={(e) => {
+                    this.setState({ txtPassword: e.target.value })
+                  }} /></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td><input type="submit" value="LOGIN" onClick={(e) => this.btnLoginClick(e)}
+                  /></td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </div>
+      );
+    }
+    return (<div />);
+  }
+  // event-handlers
+  btnLoginClick(e) {
+    e.preventDefault();
+    const username = this.state.txtUsername;
+    const password = this.state.txtPassword;
+    if (username && password) {
+      const account = { username: username, password: password };
+      this.apiLogin(account);
+    } else {
+      alert('Please input username and password');
+    }
+  }
+  // apis
+  apiLogin(account) {
+    axios.post('/api/admin/login', account).then((res) => {
+      const result = res.data;
+      if (result.success === true) {
+        this.context.setToken(result.token);
+        this.context.setUsername(account.username);
+      } else {
+        alert(result.message);
+      }
+    });
+  }
+}
+export default Login;
