3471866aaa7d96d5ff30e8a48d723ae55daedffe
JohndayN <nguyengiap211004@gmail.com>
2025-03-22 11:15:21 +0700

    A Whole Mess

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 // Sử dụng middleware
-app.use(express.json()); // Thêm middleware để xử lý JSON
-app.use(express.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 
 // Routes API    
@@ -16,6 +17,19 @@ app.get('/', (req, res) => {
 // APIs
 app.use('/api/admin', require('./api/admin.js'));
 app.use('/api/customer', require('./api/customer.js'));
+
+// deployment
+const path = require('path');
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
 
 // Start server
 app.listen(PORT, () => {
